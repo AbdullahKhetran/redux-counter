8a1e044ded286d8f7cde643617d99095d8f2fb2f
Abdullah Khetran <khetranabdullah@gmail.com>
2023-08-22 23:31:18 +0500

    applied persistor

--- redux/store.ts
+++ redux/store.ts
@@ -1,12 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import counterReducer from "./features/counter/counterSlice"
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage"
 
+const persistConfig = {
+    key: "root",
+    storage
+}
+
+const combinedReducer = combineReducers({
+    counter: counterReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, combinedReducer)
 
 export const store = configureStore({
-    reducer: {
-        counter: counterReducer
-    }
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false
+    }),
+    devTools: true
 })
+
+export const persistor = persistStore(store)
+
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
